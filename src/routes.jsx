891855ffa1cb999fbf55575a128a75753959c247
Jeff Knox <knoxjeffrey@outlook.com>
2015-09-23 10:41:38 +0100

    change to routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,6 +3,7 @@ var ReactRouter = require('react-router');
 var History = require('react-router/node_modules/history/lib/createBrowserHistory');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
+var IndexRoute = ReactRouter.IndexRoute
 
 var Main = require("./components/main");
 var Home = require("./components/home");
@@ -10,9 +11,9 @@ var Treatment = require("./components/treatment");
 var Contact = require("./components/contact");
 
 module.exports = (
-  <Router history={new History}>
-    <Route component={Main} >
-      <Route path="/" component={Home} />
+  <Router history={History()}>
+    <Route path='/' component={Main} >
+      <IndexRoute component={Home} />
       <Route path="/treatment" component={Treatment} />
       <Route path="/contact" component={Contact} />
     </Route>
